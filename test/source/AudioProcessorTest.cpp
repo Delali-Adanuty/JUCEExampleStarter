9903853edd13d8694a30ef0e988fc45b82c0433e
Van-Dyck Adanuty <adanutyv@msu.edu>
2026-06-06 18:55:44 -0400

    Set up project with tests

---
+++ test/source/AudioProcessorTest.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "Example/PluginProcessor.h"
+namespace audio_plugin_test {
+    TEST(AudioPluginAudioProcessor, Foo){
+        AudioPluginAudioProcessor processor{};
+        ASSERT_TRUE(true);
+    }
+}
